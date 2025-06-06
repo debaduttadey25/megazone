88d6582a987b37bd56325e1d51d8614a103f9acf
DEBADUTTA DEY <75461330+debaduttadey25@users.noreply.github.com>
2025-06-07 02:47:17 +0530

    Initial commit

---
+++ src/components/landigpage.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+
+function LandingPage(){
+
+
+
+    return (
+
+        <div className='p-d-landingpage'>
+            <div>
+        <span>Hello World</span>
+            </div>
+        
+        
+        </div>
+    )
+
+
+}
+export default LandingPage
