62b4127469708a8e720705d1b367070da64b87eb
debaduttadey25 <debaduttadey2019@gmail.com>
2025-06-07 03:33:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 import LandingPage from "./components/landigpage";
-import { Router,Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; 
 
 function App() {
   return (
     <div className="App">
+   
       <Router>
-        {/* Define Routes for different pages */}
-        <Route  path="/" component={LandingPage} />
-        
+        <Routes>
+          
+          <Route path="/" element={<LandingPage />} /> 
+        </Routes>
       </Router>
     </div>
   );
